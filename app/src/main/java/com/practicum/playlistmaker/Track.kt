3837eefbcc02fa5d669d1b56463ef8304a498c92
Roman <kalinroman034@gmail.com>
2023-03-03 20:16:28 +0300

    PlaylistMaker 3.0

---
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -0,0 +1,6 @@
+package com.practicum.playlistmaker
+
+data class Track(val trackName: String,
+            var artistName: String,
+            var trackTime: String,
+            var artworkUrl100: String)
